ba78700c474599eed9f75adac92a5f2f70372cc9
the-bsx <bishalbhusal005@gmail.com>
2026-06-29 18:49:27 +0545

    understanding the execution context

---
+++ execution/01.js
@@ -0,0 +1,10 @@
+// execution context in js
+
+    let val1 = 10;
+    let val2 = 5;
+    function addNum(num1, num2) {
+        let total = num1 + num2;
+        return total
+    }
+    let res1 = addNum(val1, val2);
+    let res2 = addNum(10, 2)
