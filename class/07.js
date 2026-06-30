4b74aef2f61aa1732098e49ed334d507a06e3e91
the-bsx <bishalbhusal005@gmail.com>
2026-06-30 21:41:16 +0545

    getter setter  ,bind  static property

---
+++ class/07.js
@@ -0,0 +1,25 @@
+// getter and setter in js
+
+class User{
+    constructor(name, email){
+        this.name = name;
+        this.email = email
+    }
+
+    get name(){
+        return this._name;
+    }
+    set name(value) {
+        this._name = value;
+    }
+    get email(){
+        return `${this._email}#123`
+    }
+    set email(value){
+        this._email = value;
+    }
+}
+
+const user = new User("Bishal", "bishal@example.com");
+console.log(user.name)
+console.log(user.email)
